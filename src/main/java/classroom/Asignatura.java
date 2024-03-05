9fb6228c8a0e0e85a244598a0f4dd07a8d0c02e6
jluissucerquia <jsucerquia@unal.edu.co>
2024-03-05 09:28:19 -0500

    .

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -62,4 +62,18 @@ public class Asignatura {
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
+    public enum Tipo {
+    	DISCIPLINAR(10, "DISCIPLINAR"), FUNDAMENTACION(20, "FUNDAMENTACION"), ELECTIVA(30, "ELECTIVA");
+    	
+    	private int codigo;
+    	private String nombre;
+    	
+    	Tipo(int codigo,String nombre) {
+    		this.codigo = codigo;
+    		this.nombre = nombre;
+    		
+    	}
+    }
+
 }
+
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -51,15 +51,3 @@ public class Persona {
     }
 }
 
-public enum Tipo {
-	DISCIPLINAR(10, "DISCIPLINAR"), FUNDAMENTACION(20, "FUNDAMENTACION"), ELECTIVA(30, "ELECTIVA");
-	
-	private int codigo;
-	private String nombre;
-	
-	Tipo(int codigo,String nombre) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-		
-	}
-}
