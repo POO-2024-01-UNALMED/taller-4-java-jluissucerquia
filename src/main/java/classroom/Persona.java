f3dc2ee52af1c70baf37655102c7bfd0d7135c9b
jluissucerquia <jsucerquia@unal.edu.co>
2024-03-05 09:30:23 -0500

    .

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -49,5 +49,18 @@ public class Persona {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    public enum Tipo {
+    	DISCIPLINAR(10, "DISCIPLINAR"), FUNDAMENTACION(20, "FUNDAMENTACION"), ELECTIVA(30, "ELECTIVA");
+    	
+    	private int codigo;
+    	private String nombre;
+    	
+    	Tipo(int codigo,String nombre) {
+    		this.codigo = codigo;
+    		this.nombre = nombre;
+    		
+    	}
+    }
+
 }
 
